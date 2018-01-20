daff809b7b14b52523d323b8d1cad5c8ee381370
Michael Oltmanns <mikeolt7@gmail.com>
2018-01-20 15:32:25 -0800

    Added project, added some test items based off of examples

---
+++ Items/BombRain.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SealModSix.Items
+{
+    public class BombRain : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Bomb Rain");
+            Tooltip.SetDefault("Destroy the world!");
+        }
+        public override void SetDefaults()
+        {
+            item.damage = 100;
+            item.melee = true;
+            item.width = 120;
+            item.height = 120;
+            item.useTime = 10;
+            item.useAnimation = 10;
+            item.useStyle = 1;
+            item.knockBack = 6;
+            item.shoot = 470;
+            item.shootSpeed = 8f;
+            item.value = 10000;
+            item.rare = 11;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DirtBlock, 10);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            if (Main.rand.Next(3) == 0)
+            {
+                //Emit dusts when swing the sword
+                Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, mod.DustType("Sparkle"));
+            }
+        }
+
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
+        {
+            // Add Onfire buff to the NPC for 1 second
+            // 60 frames = 1 second
+            target.AddBuff(BuffID.OnFire, 60);
+        }
+
+        // This is dangerous. I've created too many stars. Oh well
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Vector2 target = Main.screenPosition + new Vector2((float)Main.mouseX, (float)Main.mouseY);
+            float ceilingLimit = target.Y;
+            if (ceilingLimit > player.Center.Y - 200f)
+            {
+                ceilingLimit = player.Center.Y - 200f;
+            }
+            for (int i = 0; i < 32; i++)
+            {
+                position = player.Center + new Vector2((-(float)Main.rand.Next(-2401, 2401) * player.direction), -600f);
+                position.Y -= (100 * i);
+                Vector2 heading = target - position;
+                if (heading.Y < 0f)
+                {
+                    heading.Y *= -1f;
+                }
+                if (heading.Y < 20f)
+                {
+                    heading.Y = 20f;
+                }
+                heading.Normalize();
+                heading *= new Vector2(speedX, speedY).Length();
+                speedX = heading.X;
+                speedY = heading.Y + Main.rand.Next(-40, 41) * 0.02f;
+                Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage * 2, knockBack, player.whoAmI, 0f, ceilingLimit);
+            }
+            return false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SealModSix
-A Terraria mod that my friends and I made to have fun with.
+A Terraria mod that my friends and I made to have fun with. We do what we want with it. We'll see where it goes.
+
+The name is an inside joke. 
